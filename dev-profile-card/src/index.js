39beda19b5fa19f2cfd8de502bd9f696c7b3cf16
Hussain Ali <huali235@gmail.com>
2023-06-23 17:30:42 -0400

    rendering list using map method

--- dev-profile-card/src/index.js
+++ dev-profile-card/src/index.js
@@ -2,6 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ".//styles.css";
 
+const skillList = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#1c7ed6",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#fcc419",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#a9e34b",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#fa5252",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#3bc9db",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#ffa94d",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -30,22 +63,31 @@ function Intro() {
 }
 
 function SkillList() {
+  const skills = skillList;
   return (
     <ul className="skill-list">
-      <Skill skill="HTML+CSS" emoji="💪" color="#1c7ed6" />
+      {skills.map((skill) => (
+        <Skill skillObj={skill} key={skill.skill} />
+      ))}
+      {/* <Skill skill="HTML+CSS" emoji="💪" color="#1c7ed6" />
       <Skill skill="JavaScript" emoji="💪" color="#fcc419" />
       <Skill skill="Web Design" emoji="💪" color="#a9e34b" />
       <Skill skill="Git and Github" emoji="👍" color="#fa5252" />
       <Skill skill="React" emoji="💪" color="#3bc9db" />
-      <Skill skill="Svelte" emoji="👶" color="#ffa94d" />
+      <Skill skill="Svelte" emoji="👶" color="#ffa94d" /> */}
     </ul>
   );
 }
 
-function Skill(props) {
+function Skill({ skillObj }) {
+  const levels = {
+    advanced: "💪",
+    intermediate: "👍",
+    beginner: "👶",
+  };
   return (
-    <li style={{ backgroundColor: `${props.color}` }}>
-      {props.skill} {props.emoji}
+    <li style={{ backgroundColor: `${skillObj.color}` }}>
+      {skillObj.skill} {levels[skillObj.level]}
     </li>
   );
 }
