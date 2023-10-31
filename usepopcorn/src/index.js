759b395413c56e01fe85a632a24c400ec8dd2fa5
Hussain Ali <huali235@gmail.com>
2023-10-31 11:57:56 -0400

    return to App comp

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// import StarRating from "./StarRating";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={30}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    /> */}
   </React.StrictMode>
 );
