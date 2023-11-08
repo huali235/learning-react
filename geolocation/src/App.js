1d669658cc2538cda207f03584c782f4d7fad788
Hussain Ali <huali235@gmail.com>
2023-11-08 10:00:54 -0500

    refractor code

--- geolocation/src/App.js
+++ geolocation/src/App.js
@@ -1,40 +1,23 @@
 import { useState } from "react";
-
-// function useGeolocation() {}
+import { useGeolocation } from "./useGeolocation";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
   const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
+  const {
+    isLoading,
+    error,
+    getPosition,
+    position: { lat, lng },
+  } = useGeolocation();
 
-  const { lat, lng } = position;
-
-  function getPosition() {
+  function handleClick() {
     setCountClicks((count) => count + 1);
-
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
+    getPosition();
   }
 
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
+      <button onClick={handleClick} disabled={isLoading}>
         Get my position
       </button>
 
